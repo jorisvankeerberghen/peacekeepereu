3e1b799655beb0ca5c3bc911e8b3e0e0b91efcab
jorisvankeerberghen <joris.vankeerberghen@gmail.com>
2024-03-10 22:08:56 +0100

    change to EN

--- myapp.py
+++ myapp.py
@@ -11,7 +11,7 @@ def home_page():
     st.image(image_url)
 
 def page1():
-    st.title("Analyse des données")
+    st.title("Data Analysis")
     st.write("This is page 1.")
 
 def page2():
@@ -21,12 +21,12 @@ def page2():
 
 # Use a sidebar for navigation between pages
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ('Home', 'Analyse des données', 'Travel authorisation'))
+page = st.sidebar.radio("Go to", ('EU Peace Keeper Project', 'Data Analysis', 'Travel authorisation'))
 
 # Display the selected page with the radio buttons
-if page == 'Home':
+if page == 'EU Peace Keeper Project':
     home_page()
-elif page == 'Analyse des données':
+elif page == 'Data Analysis':
     page1()
 elif page == 'Travel authorisation':
     page2()
